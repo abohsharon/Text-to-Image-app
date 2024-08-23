053b244730b4bb372cbbce5e91410ca20efce716
abohsharon <100698006+abohsharon@users.noreply.github.com>
2024-08-23 14:28:11 +0100

    Final Commit

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { ColorEvent } from 'ngx-color';
 import { ColorChromeModule } from 'ngx-color/chrome';
 
 @Component({
@@ -13,8 +14,15 @@ export class GeneratorComponent {
 
  @ViewChild('canva', {static: false})  myCanvas: any;
 
+ topText:string = '';
+ bottomText:string = '';
+ fileEvent:any;
+ textColor: string = '#000000'; 
+ backgroundColor: string = '#F9F9FB';
 
   preview(e:any){
+
+    this.fileEvent = e;
     let canvas = this.myCanvas.nativeElement;
     let ctx = canvas.getContext('2d');
 
@@ -25,19 +33,61 @@ export class GeneratorComponent {
     render.onload = function (event) {
      
       const img = new Image();
-        
-        img.src = render.result as string
+      console.log(img);
+      img.src = event.target?.result as string
+       
+      //   img.src = render.result as string
 
         // console.log(e.target.files)
 
       // img.src = event.target.result as string;
 
       img.onload = function (){
-        ctx.drawImage(img , 50, 50);
+        // ctx.drawImage(img , 33, 71, 104, 124, 21, 20, 87, 104);
+        ctx.drawImage(img , 20, 25, 260, 100);
       }
     }
 
   }
 
+  drawText(){
+    let canvas = this.myCanvas.nativeElement;
+    let ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    ctx.fillStyle = this.backgroundColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.preview(this.fileEvent)
+
+    ctx.fillStyle = this.textColor;
+    ctx.font = '12px Roman Italic Ms'
+    ctx.fillText(this.topText, canvas.width/3, 20)
+
+    ctx.fillText(this.bottomText, canvas.width/3, 140)
+
+  }
+
+  canvasTextColor($event: ColorEvent){
+      this.textColor = $event.color.hex;
+      this.drawText();
+  }
+
+  canvasBackgroundColor($event: ColorEvent){
+       this.backgroundColor = $event.color.hex;
+       this.drawText()
+  }
+
+  imageDownload(){
+    let canvas = this.myCanvas.nativeElement;
+    
+    let image = canvas.toDataURL('image/png');
+
+    let link = document.createElement('a');
+    link.download = 'Img.png';
+    link.href = image;
+    link.click();
+  }
    
 }
